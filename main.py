b8ab5126b52840be6781e6e9be12872d2f691b9b
Vasiliy003 <petapetkin82@gmail.com>
2025-05-24 12:58:52 +0700

    dd

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ bot_token = "8127107082:AAFfWLCYLgtjKaWmLuoCBjqi1nl_g4V4zJU"  # замените
 
 app = Client("simple_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-@app.on_message(filters.text)
-def reply_a(client, message):
-    message.reply_text("а")
-
 @app.on_message(filters.command("start"))
 def start(client, message):
     kb = ReplyKeyboardMarkup(
         [[KeyboardButton("Открыть WebApp", web_app=WebAppInfo(url="https://https://vasiliy003.github.io/Prompt/"))]]
     )
     message.reply("Нажмите кнопку ниже:", reply_markup=kb)
+
+@app.on_message(filters.text)
+def reply_a(client, message):
+    message.reply_text("а")
 
 app.run()
